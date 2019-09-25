8551644ca93e6028e2ccfeb82eb6b787f28283d7
Jonathan.e <jonathanerquinigo@pursuit.org>
2019-09-25 17:49:40 -0400

    adding initial logic for the audio player

---
+++ index.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", ()=> {
+    let main = document.getElementById("mainDiv")
+    let button = document.getElementById("button")
+    let button2 = document.getElementById("button2")
+    let button3 = document.getElementById("button3")
+
+    let songList = ["./music/siamesMrFear.mp3", "./music/jackStauberButtercup.mp3", "./music/phumLongGone.mp3"]
+    let currentSong = 0
+
+        let song = new Audio()
+        song.controls = true
+        song.src= songList[currentSong]
+        // main.appendChild(song)
+
+        button.addEventListener("click", e => {
+           
+                song.play()
+
+        })
+
+        button2.addEventListener("click", e => {
+           
+            song.pause()
+    })
+
+    button3.addEventListener("click", e => {
+        currentSong++
+        song.src= songList[currentSong]
+        if(currentSong > 2){
+            currentSong = 0
+        }
+        song.play()
+})
+
+})
